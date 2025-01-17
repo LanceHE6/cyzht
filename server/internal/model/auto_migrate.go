26181444c7ae46ad44063cb593aa9ecb543ca1b8
LanceHE6 <2765543491@qq.com>
2025-01-17 20:34:27 +0800

    chore:优化项目结构

---
+++ server/internal/model/auto_migrate.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// CreateTable
+//
+//	@Description: 创建表
+//	@param db *gorm.db 数据库连接
+func CreateTable(db *gorm.DB) {
+	db.AutoMigrate(&UserModel{})
+	db.AutoMigrate(&ActivityModel{})
+}
