416053477e8b0783fdad3f2985b14de50bc9d7c7
LanceHE6 <2765543491@qq.com>
2025-03-01 14:15:56 +0800

    refactor(components): 重构导航栏组件
    - 移除了未使用的导入和图标
    - 更新了网站名称为"重邮展会通"- 删除了导航栏中的社交媒体链接和赞助按钮
    - 注释掉了导航栏菜单相关代码

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@nextui-org/button";
 import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
@@ -8,21 +7,13 @@ import {
   NavbarContent,
   NavbarItem,
   NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { link as linkStyles } from "@nextui-org/theme";
 import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-} from "@/components/icons";
+import { GithubIcon, SearchIcon } from "@/components/icons";
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
@@ -57,7 +48,7 @@ export const Navbar = () => {
             href="/"
           >
             <Logo />
-            <p className="font-bold text-inherit">重邮展慧通</p>
+            <p className="font-bold text-inherit">重邮展会通</p>
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
@@ -83,30 +74,12 @@ export const Navbar = () => {
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
           <Link isExternal href={siteConfig.links.github} title="GitHub">
             <GithubIcon className="text-default-500" />
           </Link>
           <ThemeSwitch />
         </NavbarItem>
         <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -117,28 +90,28 @@ export const Navbar = () => {
         <NavbarMenuToggle />
       </NavbarContent>
 
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
+      {/*<NavbarMenu>*/}
+      {/*  {searchInput}*/}
+      {/*  <div className="mx-4 mt-2 flex flex-col gap-2">*/}
+      {/*    {siteConfig.navMenuItems.map((item, index) => (*/}
+      {/*      <NavbarMenuItem key={`${item}-${index}`}>*/}
+      {/*        <Link*/}
+      {/*          color={*/}
+      {/*            index === 2*/}
+      {/*              ? "primary"*/}
+      {/*              : index === siteConfig.navMenuItems.length - 1*/}
+      {/*                ? "danger"*/}
+      {/*                : "foreground"*/}
+      {/*          }*/}
+      {/*          href="#"*/}
+      {/*          size="lg"*/}
+      {/*        >*/}
+      {/*          {item.label}*/}
+      {/*        </Link>*/}
+      {/*      </NavbarMenuItem>*/}
+      {/*    ))}*/}
+      {/*  </div>*/}
+      {/*</NavbarMenu>*/}
     </NextUINavbar>
   );
 };
