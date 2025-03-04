4b12c4d4d5b48d65c43f637ea3c6130cd69c6eb2
LanceHE6 <2765543491@qq.com>
2025-03-04 15:47:21 +0800

    refactor(user): 修改用户性别字段定义
    - 将性别字段的含义从 0:男 1:女 -1:未知 更改为 1:男 2:女 -1:保密
    - 这样可以避免零值的不确定性，提高数据的一致性和可读性

--- server/internal/model/user_model.go
+++ server/internal/model/user_model.go
@@ -9,7 +9,7 @@ type UserModel struct {
 	Password string `gorm:"column:password;type:varchar(255);not null" json:"-"`             // 密码
 	Nickname string `gorm:"column:nickname;type:varchar(255);not null" json:"nickname"`      // 姓名
 	Avatar   string `gorm:"column:avatar;type:varchar(255);" json:"avatar"`                  // mongodb头像ID
-	Sex      int    `gorm:"column:sex;type:int;default -1" json:"sex"`                       // 性别0:男 1:女 -1:未知
+	Sex      int    `gorm:"column:sex;type:int;default -1" json:"sex"`                       // 性别1:男 2:女 -1:保密 避免零值
 	//Signature    string `gorm:"column:signature;type:varchar(255);" json:"signature"`            // 个性签名
 	//OnlineStatus int    `gorm:"column:online_status;type:int;default '0'" json:"online_status"`  // 在线状态0为离线1为在线
 	SessionID string `gorm:"column:session_id;type:varchar(255)" json:"session_id"` // session_id
