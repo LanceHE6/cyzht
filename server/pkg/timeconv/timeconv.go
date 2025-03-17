3fc441f34ef9cb34921ceab78ba2913a8ae9cc19
LanceHE6 <2765543491@qq.com>
2025-03-17 13:42:00 +0800

    feat(timeconv): 支持两种时间格式的解析
    -增加了对 "2006-01-02T15:04:05" 格式的支持
    - 优化了时间格式解析的错误处理

--- server/pkg/timeconv/timeconv.go
+++ server/pkg/timeconv/timeconv.go
@@ -7,13 +7,20 @@ import (
 
 // ParesStrToTime 将时间字符串转为Time类型
 func ParesStrToTime(timeStr string) (*time.Time, error) {
-	// 定义时间的布局，必须与时间字符串的格式相匹配
-	layout := "2006-01-02 15:04:05"
-	// 使用 Parse 函数将时间字符串转换为 time.Time 类型
-	t, err := time.Parse(layout, timeStr)
-	if err != nil {
-		fmt.Println("时间转换错误：", err)
-		return nil, err
+	// 定义支持的两种时间格式
+	layouts := []string{
+		"2006-01-02 15:04:05", // 格式1: "2024-11-26 20:56:29"
+		"2006-01-02T15:04:05", // 格式2: "2025-03-18T11:45:03"
 	}
-	return &t, nil
+
+	// 尝试用每种格式解析时间字符串
+	for _, layout := range layouts {
+		t, err := time.Parse(layout, timeStr)
+		if err == nil {
+			return &t, nil // 解析成功，返回时间
+		}
+	}
+
+	// 如果所有格式都解析失败，返回错误
+	return nil, fmt.Errorf("时间格式不支持: %s", timeStr)
 }
