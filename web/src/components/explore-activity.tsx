96f581248fd9ce2ce059a7f9ac1e431d397a4a88
LanceHE6 <2765543491@qq.com>
2025-03-24 20:59:23 +0800

    feat(components): 新增 ActivityCard组件并优化 explore-activity 页面布局
    - 新增 ActivityCard 组件用于展示活动信息
    - 引入 Card、CardHeader、Image、CardFooter、Button 组件
    - 添加 formatDate 工具函数
    - 更新 explore-activity 页面布局，使用新的 ActivityCard 组件

--- web/src/components/explore-activity.tsx
+++ web/src/components/explore-activity.tsx
@@ -1,15 +1,70 @@
 // explore-activity.tsx
 import { Input } from "@heroui/input";
 import {
+  Card,
+  CardHeader,
   ScrollShadow,
   Spacer,
+  Image,
+  CardFooter,
+  Button,
 } from "@heroui/react";
 import { useEffect, useState } from "react";
 
 import { SearchIcon } from "@/components/icons.tsx";
 import { axiosInstance } from "@/utils/axios-instance.ts";
 import { Toast } from "@/utils/utils.ts";
-import { ActivityCard } from "@/components/activity-card.tsx"; // 引入新创建的组件
+import { formatDate } from "@/utils/datetime.ts";
+
+interface ActivityCardProps {
+  activity: any;
+}
+
+// ActivityCard 展会展示卡片
+const ActivityCard = ({ activity }: ActivityCardProps) => {
+  return (
+    <Card key={activity.id} isFooterBlurred className="py-1">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <h4 className="font-bold text-large">{activity.name}</h4>
+        <p className="text-tiny uppercase font-bold">
+          时间: {formatDate(activity.start_at)} - {formatDate(activity.end_at)}
+        </p>
+      </CardHeader>
+      <Spacer y={2} />
+      <Image
+        removeWrapper
+        alt="Card background"
+        className="object-cover rounded-xl h-full"
+        src={
+          activity.avatar
+            ? activity.avatar
+            : "https://heroui.com/images/hero-card.jpeg"
+        }
+      />
+      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100 h-1/5">
+        <div className="flex flex-grow gap-2 items-center">
+          <Image
+            alt="activity icon"
+            className="rounded-full w-10 h-11 bg-black"
+            src={
+              activity.creator.avatar
+                ? activity.creator.avatar
+                : "https://heroui.com/images/hero-card.jpeg"
+            }
+          />
+          <div className="flex flex-col">
+            <p className="text-tiny text-white/60">
+              {activity.creator.nickname}
+            </p>
+          </div>
+        </div>
+        <Button radius="full" size="sm">
+          查看
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export const ExploreActivity = () => {
   // 展会数据
