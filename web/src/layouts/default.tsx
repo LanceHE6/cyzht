be74998a043f3729e77f03743cd14fcdeab67244
LanceHE6 <2765543491@qq.com>
2025-03-24 20:59:36 +0800

    refactor(layouts): 注释掉 Navbar 组件的导入和使用- 注释掉了 Navbar 组件的导入语句 - 移除了 DefaultLayout 组件中的 Navbar 渲染

--- web/src/layouts/default.tsx
+++ web/src/layouts/default.tsx
@@ -1,6 +1,6 @@
 // import { Link } from "@heroui/link";
 
-import { Navbar } from "@/components/navbar";
+// import { Navbar } from "@/components/navbar";
 
 export default function DefaultLayout({
   children,
@@ -9,7 +9,6 @@ export default function DefaultLayout({
 }) {
   return (
     <div className="relative flex flex-col h-screen">
-      <Navbar />
       <main className="container mx-auto max-w-7xl px-6 flex-grow">
         {children}
       </main>
