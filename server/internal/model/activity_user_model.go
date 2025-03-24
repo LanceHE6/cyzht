5f5fecdd14efd559a64b34557d4a6b996edcb9a3
LanceHE6 <2765543491@qq.com>
2025-03-24 20:52:08 +0800

    refactor(model): 优化模型结构和 JSON 字段
    -调整 ActivityModel 中 CreatorID 和 Creator 的 JSON 标签
    - 修改 ActivityUserModel 中的字段可见性
    - 更新 ExhibitorModel 中的关联关系标签
    - 重构 MetaMsg 和 MsgModel，改善消息模型结构

--- server/internal/model/activity_user_model.go
+++ server/internal/model/activity_user_model.go
@@ -3,10 +3,10 @@ package model
 // ActivityUserModel 用户参与活动绑定模型
 type ActivityUserModel struct {
 	BaseModel
-	ActivityID int64         `gorm:"column:activity_id;type:bigint;not null" json:"activity_id"`
-	Activity   ActivityModel `gorm:"foreignKey:ActivityID;references:ID" json:"activity"`
-	UserID     int64         `gorm:"column:user_id;type:bigint;not null" json:"user_id,string"`
-	User       UserModel     `gorm:"foreignKey:UserID;references:ID" json:"-"`
+	ActivityID int64          `gorm:"column:activity_id;type:bigint;not null" json:"-"`
+	Activity   *ActivityModel `gorm:"foreignKey:ActivityID;references:ID" json:"activity"`
+	UserID     int64          `gorm:"column:user_id;type:bigint;not null" json:"-"`
+	User       *UserModel     `gorm:"foreignKey:UserID;references:ID" json:"-"`
 }
 
 func (*ActivityUserModel) TableName() string {
--- server/internal/model/msg_model.go
+++ server/internal/model/msg_model.go
@@ -9,19 +9,23 @@ const (
 
 // MetaMsg 消息元数据
 type MetaMsg struct {
-	UserID   int64   `gorm:"column:user_id;type:bigint;not null" json:"user_id,string"`
-	SendTo   int64   `gorm:"column:send_to;type:bigint;not null" json:"send_to,string"`
-	MsgType  MsgType `gorm:"column:msg_type;type:tinyint;not null" json:"msg_type"`
-	TextMsg  string  `gorm:"column:text_msg;type:text;not null" json:"text_msg"`
-	FileURL  string  `gorm:"column:file_url;type:varchar(255);not null" json:"file_url"`
-	FileSize int64   `gorm:"column:file_size;type:bigint;not null" json:"file_size,string"`
+	FromUID  int64      `gorm:"column:from_uid;type:bigint;not null" json:"-,string"`
+	FromUser *UserModel `gorm:"foreignKey:FromUID;references:ID" json:"from_user"`
+	ToUID    int64      `gorm:"column:to_uid;type:bigint;not null" json:"-,string"`
+	ToUser   *UserModel `gorm:"foreignKey:ToUID;references:ID" json:"to_user"`
+	MsgType  MsgType    `gorm:"column:msg_type;type:tinyint;not null" json:"msg_type"`
+	TextMsg  string     `gorm:"column:text_msg;type:text;not null" json:"text_msg"`
+	FileURL  string     `gorm:"column:file_url;type:varchar(255);not null" json:"file_url"`
+	FileSize int64      `gorm:"column:file_size;type:bigint;not null" json:"file_size,string"`
 }
 
 // MsgModel 聊天消息表
 type MsgModel struct {
 	BaseModel
-	ActivityID  int64 `gorm:"column:activity_id;type:bigint;not null" json:"activity_id"`
-	ExhibitorID int64 `gorm:"column:exhibitor_id;type:bigint;not null" json:"exhibitor_id"` // 展商ID, 若为0，则表示是活动大厅聊天消息
+	ActivityID  int64           `gorm:"column:activity_id;type:bigint;not null" json:"-"`
+	Activity    *ActivityModel  `gorm:"foreignKey:ActivityID;references:ID" json:"activity"`
+	ExhibitorID int64           `gorm:"column:exhibitor_id;type:bigint;not null" json:"-"` // 展商ID, 若为0，则表示是活动大厅聊天消息
+	Exhibitor   *ExhibitorModel `gorm:"foreignKey:ExhibitorID;references:ID" json:"exhibitor"`
 	MetaMsg
 }
 
