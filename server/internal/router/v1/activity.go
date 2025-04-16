7a2e8a5df1705b50fb905194c74746ec2ad5157b
LanceHE6 <2765543491@qq.com>
2025-04-16 21:07:06 +0800

    refactor(chat):移除chat相关路由

--- server/internal/router/v1/activity.go
+++ server/internal/router/v1/activity.go
@@ -3,7 +3,6 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"server/internal/handler/activity"
-	"server/internal/handler/chat"
 	"server/internal/middleware"
 )
 
@@ -13,14 +12,11 @@ import (
 //	@param group *gin.RouterGroup 路由组
 func RegisterActivityRouter(group *gin.RouterGroup,
 	activityHandler activity.HandlerInterface,
-	chatHandler chat.HandlerInterface,
 ) {
 	routerGroup := group.Group("/activity")
 	routerGroup.POST("/add", middleware.Auth(), activityHandler.AddActivity)
 	routerGroup.DELETE("/del", middleware.Auth(), activityHandler.DeleteActivity)
 	routerGroup.GET("/search", activityHandler.SearchActivity)
-	routerGroup.POST("/:aid/send", middleware.Auth(), chatHandler.SendToActivity)
-	routerGroup.GET("/:aid/msg", middleware.Auth(), chatHandler.GetActivityMsg)
 	routerGroup.POST("/:aid/join", middleware.Auth(), activityHandler.JoinActivity)
 	routerGroup.POST("/:aid/withdraw", middleware.Auth(), activityHandler.WithdrawActivity)
 	routerGroup.GET("/joined", middleware.Auth(), activityHandler.GetJoinedActivity)
